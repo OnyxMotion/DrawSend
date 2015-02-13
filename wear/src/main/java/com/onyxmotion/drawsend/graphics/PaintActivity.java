a46ebc964fe861a3c72be7882b3facd5035f5a38
Vivek <vivekkesarwani91@gmail.com>
2015-02-13 17:06:39 -0500

    More changes

---
+++ wear/src/main/java/com/onyxmotion/drawsend/graphics/PaintActivity.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2007 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.onyxmotion.drawsend.graphics;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.*;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.onyxmotion.drawsend.R;
+
+public class PaintActivity extends Activity
+        implements ColorPickerDialog.OnColorChangedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paint);
+
+		paintView = (PaintView) findViewById(R.id.paint_view);
+
+        mEmboss = new EmbossMaskFilter(new float[] { 1, 1, 1 },
+                                       0.4f, 6, 3.5f);
+
+        mBlur = new BlurMaskFilter(8, BlurMaskFilter.Blur.NORMAL);
+    }
+
+	private PaintView paintView;
+    private MaskFilter  mEmboss;
+    private MaskFilter  mBlur;
+
+    public void colorChanged(int color) {
+        paintView.setColor(color);
+    }
+
+    private static final int COLOR_MENU_ID = Menu.FIRST;
+    private static final int EMBOSS_MENU_ID = Menu.FIRST + 1;
+    private static final int BLUR_MENU_ID = Menu.FIRST + 2;
+    private static final int ERASE_MENU_ID = Menu.FIRST + 3;
+    private static final int SRCATOP_MENU_ID = Menu.FIRST + 4;
+
+
+}
---
+++ wear/src/main/java/com/onyxmotion/drawsend/graphics/PaintView.java
@@ -0,0 +1,108 @@
+package com.onyxmotion.drawsend.graphics;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by Vivek on 2015-02-13.
+ */
+public class PaintView extends View {
+
+	private static final float MINP = 0.25f;
+	private static final float MAXP = 0.75f;
+
+	private Paint mPaint;
+	private Bitmap mBitmap;
+	private Canvas mCanvas;
+	private Path mPath;
+	private Paint mBitmapPaint;
+
+	public PaintView(Context c) {
+		super(c);
+
+
+		mPaint = new Paint();
+		mPaint.setAntiAlias(true);
+		mPaint.setDither(true);
+		mPaint.setColor(0xFFFF0000);
+		mPaint.setStyle(Paint.Style.STROKE);
+		mPaint.setStrokeJoin(Paint.Join.ROUND);
+		mPaint.setStrokeCap(Paint.Cap.ROUND);
+		mPaint.setStrokeWidth(12);
+		mPath = new Path();
+		mBitmapPaint = new Paint(Paint.DITHER_FLAG);
+	}
+
+	@Override
+	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+		super.onSizeChanged(w, h, oldw, oldh);
+		mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+		mCanvas = new Canvas(mBitmap);
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		canvas.drawColor(0xFFFFFF);
+
+		canvas.drawBitmap(mBitmap, 0, 0, mBitmapPaint);
+
+		canvas.drawPath(mPath, mPaint);
+	}
+
+	private float mX, mY;
+	private static final float TOUCH_TOLERANCE = 4;
+
+	public void setColor(int color) {
+		mPaint.setColor(color);
+	}
+
+	private void touch_start(float x, float y) {
+		mPath.reset();
+		mPath.moveTo(x, y);
+		mX = x;
+		mY = y;
+	}
+	private void touch_move(float x, float y) {
+		float dx = Math.abs(x - mX);
+		float dy = Math.abs(y - mY);
+		if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
+			mPath.quadTo(mX, mY, (x + mX)/2, (y + mY)/2);
+			mX = x;
+			mY = y;
+		}
+	}
+	private void touch_up() {
+		mPath.lineTo(mX, mY);
+		// commit the path to our offscreen
+		mCanvas.drawPath(mPath, mPaint);
+		// kill this so we don't double draw
+		mPath.reset();
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		float x = event.getX();
+		float y = event.getY();
+
+		switch (event.getAction()) {
+			case MotionEvent.ACTION_DOWN:
+				touch_start(x, y);
+				invalidate();
+				break;
+			case MotionEvent.ACTION_MOVE:
+				touch_move(x, y);
+				invalidate();
+				break;
+			case MotionEvent.ACTION_UP:
+				touch_up();
+				invalidate();
+				break;
+		}
+		return true;
+	}
+}
